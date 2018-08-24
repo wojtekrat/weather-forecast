d93fba6235c6b4714c353e6e26040676a648de81
a-leja <41519670+a-leja@users.noreply.github.com>
2018-08-24 20:29:39 +0200

    Add files via upload

---
+++ docs/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const apiKey = "154ea5f473dba923e387a7ee50d218a0";
+const apiUrl = "https://tile.openweathermap.org/map";
+
+const fetchApi = ${apiUrl}/temp_new/3/30/-20.png?appid=${apiKey};
+const showMap = console.log(fetchApi)
+
+class App extends Component{
+    render(){
+        return(this.showMap)
+    };
+}
+
+export default App;
